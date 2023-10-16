a9d37b6eec819a82b7bf380c5dda36ab578d6088
sShakar <shakarkosar01@gmail.com>
2023-10-16 22:24:28 +0300

    implement getStaticProps

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '@/components/layout';
+import utilStyles from '@/styles/utils.module.scss';
+import Link from 'next/link';
+
+export default function Home() {
+	return (
+		<Layout home>
+			<Head>
+				<title>{siteTitle}</title>
+			</Head>
+			<section className={utilStyles.headingMd}>
+				<p>[Your Self Introduction]</p>
+				<p>
+					(This is a sample website - you’ll be building a site like this on{' '}
+					<Link href="posts/first-post">our Next.js tutorial</Link>.)
+				</p>
+			</section>
+		</Layout>
+	);
+}
