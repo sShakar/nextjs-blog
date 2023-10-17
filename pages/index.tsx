a6b6a0693db53638bb6b7516a4482c32c209bb0a
sShakar <shakarkosar01@gmail.com>
2023-10-17 15:39:04 +0300

    dynamic routes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
+import Link from 'next/link';
 import Head from 'next/head';
 import Layout, { siteTitle } from '@/components/layout';
 import utilStyles from '@/styles/utils.module.scss';
-import Link from 'next/link';
+import { getSortedPostsData } from '@/lib/posts';
 
-export default function Home() {
+export default function Home({ allPostsData }) {
 	return (
 		<Layout home>
 			<Head>
@@ -16,6 +17,31 @@ export default function Home() {
 					<Link href="posts/first-post">our Next.js tutorial</Link>.)
 				</p>
 			</section>
+
+			<section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+				<h2 className={utilStyles.headingLg}>Blog</h2>
+				<ul className={utilStyles.list}>
+					{allPostsData.map(({ id, date, title }) => (
+						<li className={utilStyles.listItem} key={id}>
+							{title}
+							<br />
+							{id}
+							<br />
+							{date}
+						</li>
+					))}
+				</ul>
+			</section>
 		</Layout>
 	);
+}
+
+export async function getStaticProps() {
+	const allPostsData = getSortedPostsData();
+
+	return {
+		props: {
+			allPostsData
+		}
+	};
 }
